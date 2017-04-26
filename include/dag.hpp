6b1770a66452351147cbfd2ed66d24166bc63196
Harald August Bergesen <bergesenha@gmail.com>
2017-04-26 10:38:23 +0200

    Add template dependent member variables. 	modified:   include/dag.hpp

--- include/dag.hpp
+++ include/dag.hpp
@@ -1,6 +1,23 @@
 #pragma once
 
 
+#include <vector>
+
+
 namespace helene
 {
+template <class NodeType, class EdgeType> class dag
+{
+private:
+    ////////////////////////////////////////////////////////////////////////////
+    // internal classes
+
+
+public:
+    dag() = default;
+
+private:
+    std::vector<NodeType> node_properties_;
+    std::vector<EdgeType> edge_properties_;
+};
 }
