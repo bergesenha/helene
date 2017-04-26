55d051d6f2dc219687932b89cfbeafb3384eab37
Harald August Bergesen <bergesenha@gmail.com>
2017-04-26 10:40:53 +0200

    Add some member types. 	modified:   include/dag.hpp

--- include/dag.hpp
+++ include/dag.hpp
@@ -8,6 +8,17 @@ namespace helene
 {
 template <class NodeType, class EdgeType> class dag
 {
+public:
+    ////////////////////////////////////////////////////////////////////////////
+    // public typedefs
+
+    typedef NodeType value_type;
+    typedef NodeType& reference;
+    typedef const NodeType& const_reference;
+
+    typedef typename std::vector<NodeType>::difference_type difference_type;
+    typedef typename std::vector<NodeType>::size_type size_type;
+
 private:
     ////////////////////////////////////////////////////////////////////////////
     // internal classes
