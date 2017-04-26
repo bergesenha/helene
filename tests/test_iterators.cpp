411b7c3aa916c1150332869773f009f4267e4148
Harald August Bergesen <bergesenha@gmail.com>
2017-04-26 15:01:37 +0200

    Add tests for Iterator concept. 	modified:   tests/test_iterators.cpp

--- tests/test_iterators.cpp
+++ tests/test_iterators.cpp
@@ -2,6 +2,7 @@
 
 
 #include <dag.hpp>
+#include <utility>
 
 
 TEST_CASE("persistent_iterator_ to mock vector", "[presistent_iterator_]")
@@ -22,6 +23,21 @@ TEST_CASE("persistent_iterator_ to mock vector", "[presistent_iterator_]")
         REQUIRE(*my_iter2 == 'a');
     }
 
+    SECTION("move construct iterator")
+    {
+        helene::persistent_iterator_<char, int> my_iter2(std::move(my_iter));
+
+        REQUIRE(*my_iter2 == 'a');
+    }
+
+    SECTION("copy assign iterator")
+    {
+        helene::persistent_iterator_<char, int> my_iter2(10, mock_nodes);
+
+        my_iter2 = my_iter;
+
+        REQUIRE(*my_iter2 == 'a');
+    }
 
     SECTION("add another element in mock vector")
     {
@@ -34,6 +50,21 @@ TEST_CASE("persistent_iterator_ to mock vector", "[presistent_iterator_]")
             ++my_iter;
 
             REQUIRE(*my_iter == 'b');
+        }
+    }
+
+    SECTION("another mock vector and another iterator")
+    {
+        std::vector<char> mock_nodes2{'x', 'y', 'z'};
+
+        helene::persistent_iterator_<char, int> my_iter2(1, mock_nodes2);
+
+        SECTION("swap the two iterators")
+        {
+            std::swap(my_iter, my_iter2);
+
+            REQUIRE(*my_iter2 == 'a');
+            REQUIRE(*my_iter == 'y');
         }
     }
 }
