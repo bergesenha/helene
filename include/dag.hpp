d7d5d4e8dbf3a52053fe0048d3705a91531b54bf
Harald August Bergesen <bergesenha@gmail.com>
2017-04-26 10:45:38 +0200

    Add nested class edge. 	modified:   include/dag.hpp

--- include/dag.hpp
+++ include/dag.hpp
@@ -19,10 +19,26 @@ public:
     typedef typename std::vector<NodeType>::difference_type difference_type;
     typedef typename std::vector<NodeType>::size_type size_type;
 
+
+    typedef EdgeType edge_value_type;
+    typedef EdgeType& edge_reference;
+    typedef const EdgeType& const_edge_reference;
+
+    typedef typename std::vector<EdgeType>::size_type edge_size_type;
+    typedef
+        typename std::vector<EdgeType>::difference_type edge_difference_type;
+
 private:
     ////////////////////////////////////////////////////////////////////////////
     // internal classes
 
+    // indices to properties of the associated edge
+    struct edge
+    {
+        size_type from_property;
+        size_type to_property;
+        edge_size_type edge_property;
+    };
 
 public:
     dag() = default;
