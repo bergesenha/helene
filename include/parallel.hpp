9ad4a7ed085efe7c2459329c70157610017b92cc
Harald August Bergesen <bergesenha@gmail.com>
2017-04-29 10:39:14 +0200

    Add implementation of parallel_remove. 	modified:   include/parallel.hpp 	modified:   tests/test_algorithms.cpp

--- include/parallel.hpp
+++ include/parallel.hpp
@@ -6,6 +6,9 @@
 #include <algorithm>
 #include <vector>
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-parameter"
+
 
 namespace helene
 {
@@ -20,6 +23,8 @@ remove_by_index(InputIterator1 begin,
 }
 
 
+// remove elements in same positions in both ranges according to value in first
+// range.
 template <class InputIterator1, class InputIterator2, class T>
 std::pair<InputIterator1, InputIterator2>
 parallel_remove(InputIterator1 begin1,
@@ -27,9 +32,57 @@ parallel_remove(InputIterator1 begin1,
                 InputIterator2 begin2,
                 const T& value)
 {
+    auto found1 = begin1;
+
+    auto temp_end1 = end1;
+    auto temp_end2 = begin2;
+    std::advance(temp_end2, std::distance(begin1, end1));
+
+    while(found1 != end1)
+    {
+        found1 = std::find(begin1, end1, value);
+
+        temp_end1 = std::remove_if(
+            begin1,
+            temp_end1,
+            [found1](
+                typename std::iterator_traits<InputIterator1>::reference elm) {
+                if(&elm == std::addressof(*found1))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            });
+
+        auto found2 = begin2;
+        std::advance(found2, std::distance(begin1, found1));
+
+        temp_end2 = std::remove_if(
+            begin2,
+            temp_end2,
+            [found2](
+                typename std::iterator_traits<InputIterator2>::reference elm) {
+
+                if(&elm == std::addressof(*found2))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            });
+    }
+
+    return std::make_pair(temp_end1, temp_end2);
 }
 
 
+// remove elements in both ranges according to predicate on first range.
 template <class InputIterator1, class InputIterator2, class UnaryPredicate>
 std::pair<InputIterator1, InputIterator2>
 parallel_remove_if(InputIterator1 begin1,
@@ -39,3 +92,4 @@ parallel_remove_if(InputIterator1 begin1,
 {
 }
 }
+
--- tests/test_algorithms.cpp
+++ tests/test_algorithms.cpp
@@ -7,6 +7,15 @@
 TEST_CASE("test parallel_remove", "[parallel_remove]")
 {
 
+    std::vector<int> intrange{1, 2, 3, 4};
+    std::vector<char> charange{'a', 'b', 'c', 'd'};
 
+    auto rem_it = helene::parallel_remove(
+        intrange.begin(), intrange.end(), charange.begin(), 3);
 
+    intrange.erase(rem_it.first, intrange.end());
+    charange.erase(rem_it.second, charange.end());
+
+    REQUIRE(intrange.size() == 3);
+    REQUIRE(charange.size() == 3);
 }
