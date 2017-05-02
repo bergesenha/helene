78a46f9f9e1bf4ba7d3f75951d73d074476f5b8e
Harald August Bergesen <bergesenha@gmail.com>
2017-05-02 13:03:14 +0200

    Add additional template parameter to member_iterator_base. 	modified:   include/member_iterator.hpp

--- include/member_iterator.hpp
+++ include/member_iterator.hpp
@@ -6,12 +6,14 @@
 namespace helene
 {
 
-template <class IteratorCategory, class IteratorToStruct>
+template <class IteratorCategory, class IteratorToStruct, class MemberType>
 class member_iterator_base;
 
 
-template <class IteratorToStruct>
-class member_iterator_base<std::input_iterator_tag, IteratorToStruct>
+template <class IteratorToStruct, class MemberType>
+class member_iterator_base<std::input_iterator_tag,
+                           IteratorToStruct,
+                           MemberType>
 {
 public:
     member_iterator_base(IteratorToStruct it) : current_(it)
@@ -29,13 +31,18 @@ protected:
 };
 
 
-template <class IteratorToStruct>
-class member_iterator_base<std::forward_iterator_tag, IteratorToStruct>
-    : public member_iterator_base<std::input_iterator_tag, IteratorToStruct>
+template <class IteratorToStruct, class MemberType>
+class member_iterator_base<std::forward_iterator_tag,
+                           IteratorToStruct,
+                           MemberType>
+    : public member_iterator_base<std::input_iterator_tag,
+                                  IteratorToStruct,
+                                  MemberType>
 {
 public:
     using member_iterator_base<std::input_iterator_tag,
-                               IteratorToStruct>::member_iterator_base;
+                               IteratorToStruct,
+                               MemberType>::member_iterator_base;
 };
 
 
@@ -61,12 +68,15 @@ template <class MemberType,
 class member_iterator
     : public member_iterator_base<
           typename std::iterator_traits<IteratorToStruct>::iterator_category,
-          IteratorToStruct>
+          IteratorToStruct,
+          MemberType>
 {
 public:
     // using base class constructor directly
     using member_iterator_base<
         typename std::iterator_traits<IteratorToStruct>::iterator_category,
-        IteratorToStruct>::member_iterator_base;
+        IteratorToStruct,
+        MemberType>::member_iterator_base;
+
 };
 }
