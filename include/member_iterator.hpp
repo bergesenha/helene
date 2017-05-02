16e53ef71d34f30384c1781accebbce82852e708
Harald August Bergesen <bergesenha@gmail.com>
2017-05-02 12:34:11 +0200

    Decide to implement member_iterator in a tiered inherited way. 	modified:   include/member_iterator.hpp 	modified:   tests/test_member_iterator.cpp

--- include/member_iterator.hpp
+++ include/member_iterator.hpp
@@ -6,10 +6,67 @@
 namespace helene
 {
 
+template <class IteratorCategory, class IteratorToStruct>
+class member_iterator_base;
+
+
+template <class IteratorToStruct>
+class member_iterator_base<std::input_iterator_tag, IteratorToStruct>
+{
+public:
+    member_iterator_base(IteratorToStruct it) : current_(it)
+    {
+    }
+
+    bool
+    operator!=(const member_iterator_base& other) const
+    {
+        return current_ != other.current_;
+    }
+
+protected:
+    IteratorToStruct current_;
+};
+
+
+template <class IteratorToStruct>
+class member_iterator_base<std::forward_iterator_tag, IteratorToStruct>
+    : public member_iterator_base<std::input_iterator_tag, IteratorToStruct>
+{
+public:
+    using member_iterator_base<std::input_iterator_tag,
+                               IteratorToStruct>::member_iterator_base;
+};
+
+
+/*
+template <class IteratorToStruct>
+class member_iterator_base<std::random_access_iterator_tag, IteratorToStruct>
+{
+public:
+    member_iterator_base(IteratorToStruct it) : current_(it)
+    {
+    }
+
+protected:
+    IteratorToStruct current_;
+};
+*/
+
 
 template <class MemberType,
           class StructType,
-          template <class...> class Container,
+          class IteratorToStruct,
           MemberType StructType::*PtrValue>
-class member_iterator;
+class member_iterator
+    : public member_iterator_base<
+          typename std::iterator_traits<IteratorToStruct>::iterator_category,
+          IteratorToStruct>
+{
+public:
+    // using base class constructor directly
+    using member_iterator_base<
+        typename std::iterator_traits<IteratorToStruct>::iterator_category,
+        IteratorToStruct>::member_iterator_base;
+};
 }
--- tests/test_member_iterator.cpp
+++ tests/test_member_iterator.cpp
@@ -2,3 +2,54 @@
 
 
 #include <member_iterator.hpp>
+#include <vector>
+#include <forward_list>
+#include <string>
+
+
+TEST_CASE("member_iterator to member of a mock struct in std::vector",
+          "[member_iterator]")
+{
+
+
+    struct mock
+    {
+        int something;
+        double the_member;
+    };
+
+    typedef helene::member_iterator<double,
+                                    mock,
+                                    std::vector<mock>::iterator,
+                                    &mock::the_member>
+        my_iter;
+
+    std::vector<mock> container{{1, 1.0}, {2, 2.0}, {3, 3.0}};
+}
+
+
+TEST_CASE("member_iterator to member of a mock struct in a std::forward_list",
+          "[member_iterator]")
+{
+    struct mock
+    {
+        std::string name;
+        int value;
+    };
+
+    typedef helene::member_iterator<int,
+                                    mock,
+                                    std::forward_list<mock>::iterator,
+                                    &mock::value>
+        my_iter;
+
+    std::forward_list<mock> container;
+    container.push_front({std::string("one"), 1});
+    container.push_front({std::string("two"), 2});
+    container.push_front({std::string("three"), 3});
+
+    my_iter it_beg(container.begin());
+    my_iter it_end(container.end());
+
+    REQUIRE(it_beg != it_end);
+}
