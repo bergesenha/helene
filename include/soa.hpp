43909f1dbe0b55533e49def1d6ec5edd6c16b050
Harald August Bergesen <bergesenha@gmail.com>
2017-12-07 11:18:55 +0100

    Basic structure. 	modified:   include/soa.hpp 	modified:   tests/test_soa.cpp

--- include/soa.hpp
+++ include/soa.hpp
@@ -25,23 +25,43 @@ using member_type_t =
 template <class MemberPointerType>
 using object_type_t =
     typename deduce_member_pointer<MemberPointerType>::object_type;
+}
 
 
 template <class MemberPointerType, MemberPointerType MemberPointerValue>
 class member_container
 {
 public:
-    typedef member_type_t<MemberPointerType> member_type;
-    typedef object_type_t<MemberPointerType> object_type;
+    typedef soa_detail::member_type_t<MemberPointerType> member_type;
+    typedef soa_detail::object_type_t<MemberPointerType> object_type;
+
+public:
+    member_container() = default;
 
 private:
+    std::vector<member_type> members_;
 };
-}
 
 
-class soa
+// helper to access respective parents that are multiply inherited
+template <class FirstParent, class... RestParenst>
+struct parent_helper
+{
+};
+
+template <class LastParent>
+struct parent_helper<LastParent>
+{
+};
+
+template <class T, class... MemberContainer>
+class soa : public MemberContainer...
 {
 public:
+    soa() : MemberContainer::MemberContainer()...
+    {
+    }
+
 private:
 };
 }
--- tests/test_soa.cpp
+++ tests/test_soa.cpp
@@ -1,3 +1,22 @@
 #include "catch.hpp"
 
 #include <soa.hpp>
+
+
+struct point
+{
+    float x;
+    float y;
+    float z;
+};
+
+
+TEST_CASE("", "[soa]")
+{
+    typedef helene::soa<
+        point,
+        helene::member_container<decltype(&point::x), &point::x>,
+        helene::member_container<decltype(&point::y), &point::y>,
+        helene::member_container<decltype(&point::z), &point::z>>
+        mysoa;
+}
