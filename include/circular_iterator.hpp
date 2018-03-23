8b8ac91c0ef5cacab02cd6e7f70f59b21791ee0f
Harald August Bergesen <bergesenha@gmail.com>
2018-03-23 12:35:27 +0100

    Add convenience variable template in detail namespace. 	modified:   include/circular_iterator.hpp

--- include/circular_iterator.hpp
+++ include/circular_iterator.hpp
@@ -1,8 +1,20 @@
 #pragma once
 
+#include <type_traits>
+#include <iterator>
+
 
 namespace helene
 {
+namespace detail
+{
+
+template <class Iterator, class IteratorTag>
+constexpr bool is_iterator_of_category_v =
+    std::is_same<typename std::iterator_traits<Iterator>::iterator_category,
+                 IteratorTag>::value;
+
+} // namespace detail
 /** \class circular_iterator circular_iterator.hpp <circular_iterator.hpp>
  *
  * \brief An iterator adaptor that adapts any iterator to wrap around when
