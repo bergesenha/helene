4e049a3eadedd66cb65f8197a932500763fb28a3
Harald August Bergesen <bergesenha@gmail.com>
2018-03-23 12:35:46 +0100

    Add unit tests. 	modified:   CMakeLists.txt 	modified:   tests/test_circular_iterator.cpp

--- tests/test_circular_iterator.cpp
+++ tests/test_circular_iterator.cpp
@@ -1,8 +1,21 @@
-#include <catch.hpp>
+#include "catch.hpp"
+#include <vector>
 
 #include <circular_iterator.hpp>
 
 
-TEST_CASE("", "")
+TEST_CASE("test is_iterator_of_category_v for different categories",
+          "[detail::is_iterator_of_category_v]")
 {
+    typedef std::vector<int>::iterator vec_it;
+
+    constexpr bool vector_it_is_random_access = helene::detail::
+        is_iterator_of_category_v<vec_it, std::random_access_iterator_tag>;
+
+    constexpr bool vector_it_is_forward_iterator =
+        helene::detail::is_iterator_of_category_v<vec_it,
+                                                  std::forward_iterator_tag>;
+
+    CHECK(vector_it_is_random_access);
+    CHECK(!vector_it_is_forward_iterator);
 }
