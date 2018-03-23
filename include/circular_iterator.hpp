e314410cff7ab3c138b47340f88b3cbf88697bef
Harald August Bergesen <bergesenha@gmail.com>
2018-03-23 14:59:08 +0100

    Refactor base class into derived, remove base. 	modified:   include/circular_iterator.hpp

--- include/circular_iterator.hpp
+++ include/circular_iterator.hpp
@@ -9,31 +9,34 @@ namespace helene
 {
 namespace detail
 {
-
 template <class Iterator, class IteratorTag>
 constexpr bool is_iterator_of_category_v =
     std::is_same<typename std::iterator_traits<Iterator>::iterator_category,
                  IteratorTag>::value;
 
+
 template <class Iterator, class IteratorTag>
 constexpr bool is_at_least_iterator_of_category_v = std::is_base_of<
     IteratorTag,
     typename std::iterator_traits<Iterator>::iterator_category>::value;
-
-// undefined base, specialized for each iterator category
-template <class UnderlyingIterator, class = void>
-class circular_iterator_base;
+} // namespace detail
 
 
-// specialization for UnderlyingIterator satisfying ForwardIterator
+/** \class circular_iterator circular_iterator.hpp <circular_iterator.hpp>
+ *
+ * \brief An iterator adaptor that adapts any iterator to wrap around when
+ * incremented beyond a range determined on construction
+ */
 template <class UnderlyingIterator>
-class circular_iterator_base<
-    UnderlyingIterator,
-    std::enable_if_t<is_iterator_of_category_v<UnderlyingIterator,
-                                               std::forward_iterator_tag>>>
+class circular_iterator
 {
 public:
-    // inherited access to types from traits
+    static_assert(
+        detail::is_at_least_iterator_of_category_v<UnderlyingIterator,
+                                                   std::forward_iterator_tag>,
+        "UnderlyingIterator needs to at least satisfy ForwardIterator for its "
+        "multipass ability");
+
     typedef typename std::iterator_traits<UnderlyingIterator>::value_type
         value_type;
     typedef typename std::iterator_traits<UnderlyingIterator>::difference_type
@@ -45,22 +48,23 @@ public:
         iterator_category;
 
 public:
-    circular_iterator_base() = default;
+    circular_iterator() = default;
 
-    circular_iterator_base(UnderlyingIterator current) : current_(current)
+    circular_iterator(UnderlyingIterator first,
+                      UnderlyingIterator last,
+                      UnderlyingIterator current)
+        : first_(first), last_(last), current_(current)
     {
     }
 
-public:
-    // ForwardIterator conformance
     bool
-    operator==(const circular_iterator_base& other) const
+    operator==(const circular_iterator& other)
     {
         return current_ == other.current_;
     }
 
     bool
-    operator!=(const circular_iterator_base& other) const
+    operator!=(const circular_iterator& other)
     {
         return current_ != other.current_;
     }
@@ -70,54 +74,14 @@ public:
         return *current_;
     }
 
-    pointer operator->()
-    {
-        return current_.operator->();
-    }
-
-
-protected:
-    UnderlyingIterator current_;
-};
-} // namespace detail
-/** \class circular_iterator circular_iterator.hpp <circular_iterator.hpp>
- *
- * \brief An iterator adaptor that adapts any iterator to wrap around when
- * incremented beyond a range determined on construction
- */
-template <class UnderlyingIterator>
-class circular_iterator
-    : public detail::circular_iterator_base<UnderlyingIterator>
-{
-public:
-    static_assert(
-        detail::is_at_least_iterator_of_category_v<UnderlyingIterator,
-                                                   std::forward_iterator_tag>,
-        "UnderlyingIterator needs to at least satisfy ForwardIterator for its "
-        "multipass ability");
-
-public:
-    circular_iterator() = default;
-
-    circular_iterator(UnderlyingIterator first,
-                      UnderlyingIterator last,
-                      UnderlyingIterator current)
-        : detail::circular_iterator_base<UnderlyingIterator>(current),
-          first_(first),
-          last_(last)
-    {
-    }
-
     circular_iterator&
     operator++()
     {
-        ++detail::circular_iterator_base<UnderlyingIterator>::current_;
+        ++current_;
 
-        if(detail::circular_iterator_base<UnderlyingIterator>::current_ ==
-           last_)
+        if(current_ == last_)
         {
-            detail::circular_iterator_base<UnderlyingIterator>::current_ =
-                first_;
+            current_ = first_;
         }
 
         return *this;
@@ -134,5 +98,6 @@ public:
 private:
     UnderlyingIterator first_;
     UnderlyingIterator last_;
+    UnderlyingIterator current_;
 };
 } // namespace helene
