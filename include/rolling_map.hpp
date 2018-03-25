c77d95592003b7cc07afe160fbbb63f80bc8d9e6
Harald August Bergesen <bergesenha@gmail.com>
2018-03-26 01:32:10 +0200

    Add new class sliding_window_buffer. 	modified:   include/rolling_map.hpp 	modified:   tests/test_rolling_map.cpp

--- include/rolling_map.hpp
+++ include/rolling_map.hpp
@@ -1,7 +1,57 @@
 #pragma once
 
+#include <cstddef>
+
+#include "circular_iterator.hpp"
 
 namespace helene
 {
-}
+template <class T, std::size_t Size>
+class sliding_window_buffer
+{
+public:
+    typedef std::size_t size_type;
+    typedef circular_iterator<T*> iterator;
 
+public:
+    sliding_window_buffer()
+        : buffer_{},
+          origin_(std::begin(buffer_), std::end(buffer_), std::begin(buffer_))
+    {
+    }
+
+    T& operator[](size_type n)
+    {
+        return origin_[static_cast<typename iterator::difference_type>(n)];
+    }
+
+    const T& operator[](size_type n) const
+    {
+        return origin_[static_cast<typename iterator::difference_type>(n)];
+    }
+
+    void
+    push_back(const T& value)
+    {
+        origin_[Size] = value;
+        ++origin_;
+    }
+
+    void
+    push_front(const T& value)
+    {
+        --origin_;
+        *origin_ = value;
+    }
+
+    constexpr size_type
+    size() const
+    {
+        return Size;
+    }
+
+private:
+    T buffer_[Size];
+    iterator origin_;
+};
+} // namespace helene
--- tests/test_rolling_map.cpp
+++ tests/test_rolling_map.cpp
@@ -2,6 +2,45 @@
 #include <rolling_map.hpp>
 
 
-TEST_CASE("", "")
+typedef helene::sliding_window_buffer<int, 4> swb_type;
+
+TEST_CASE("default construct a sliding_window_buffer",
+          "[sliding_window_buffer]")
 {
+    swb_type swb;
+
+    CHECK(swb.size() == 4);
+
+    SECTION("push back four values")
+    {
+        swb.push_back(1);
+        swb.push_back(2);
+        swb.push_back(3);
+        swb.push_back(4);
+
+        CHECK(swb[0] == 1);
+        CHECK(swb[1] == 2);
+        CHECK(swb[2] == 3);
+        CHECK(swb[3] == 4);
+
+        SECTION("push back another value")
+        {
+            swb.push_back(5);
+
+            CHECK(swb[0] == 2);
+            CHECK(swb[1] == 3);
+            CHECK(swb[2] == 4);
+            CHECK(swb[3] == 5);
+        }
+
+        SECTION("push front a value")
+        {
+            swb.push_front(10);
+
+            CHECK(swb[0] == 10);
+            CHECK(swb[1] == 1);
+            CHECK(swb[2] == 2);
+            CHECK(swb[3] == 3);
+        }
+    }
 }
