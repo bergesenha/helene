03f86178cae2b26f2263632ba9cbafe9e86e8931
Harald August Bergesen <bergesenha@gmail.com>
2018-03-27 17:04:29 +0200

    Add unit tests for const_iterator underlying. 	modified:   tests/test_circular_iterator.cpp

--- tests/test_circular_iterator.cpp
+++ tests/test_circular_iterator.cpp
@@ -156,3 +156,145 @@ TEST_CASE("construct a circular_iterator pointing to a vector",
         CHECK(v.front() == 10);
     }
 }
+
+
+TEST_CASE("create circular_iterator with underlying iterator being vector "
+          "const_iterator",
+          "[circular_iterator]")
+{
+    typedef helene::circular_iterator<std::vector<int>::const_iterator>
+        civ_type;
+
+    const std::vector<int> v{1, 2, 3};
+
+    civ_type cit1(v.cbegin(), v.cend(), v.cbegin());
+
+    CHECK(*cit1 == 1);
+
+    CHECK(cit1[0] == 1);
+    CHECK(cit1[1] == 2);
+    CHECK(cit1[2] == 3);
+    CHECK(cit1[3] == 1);
+    CHECK(cit1[10] == 2);
+    CHECK(cit1[-1] == 3);
+    CHECK(cit1[-8] == 2);
+
+
+    SECTION("preincrement iterator")
+    {
+        auto it = ++cit1;
+        CHECK(*it == 2);
+        CHECK(*cit1 == 2);
+        CHECK(it == cit1);
+    }
+
+    SECTION("postincrement iterator")
+    {
+        auto it = cit1++;
+        CHECK(*it == 1);
+        CHECK(*cit1 == 2);
+        CHECK(it != cit1);
+    }
+
+    SECTION("assign-increment by 2")
+    {
+        cit1 += 2;
+
+        CHECK(*cit1 == 3);
+    }
+
+    SECTION("assign-increment by 4")
+    {
+        cit1 += 4;
+
+        CHECK(*cit1 == 2);
+    }
+
+    SECTION("assign-increment by 10")
+    {
+        cit1 += 10;
+
+        CHECK(*cit1 == 2);
+    }
+
+    SECTION("assign-increment by -2")
+    {
+        cit1 += -2;
+
+        CHECK(*cit1 == 2);
+    }
+
+    SECTION("assign-increment by -10")
+    {
+        cit1 += -10;
+
+        CHECK(*cit1 == 3);
+    }
+
+    SECTION("assign-decrement by 3")
+    {
+        cit1 -= 3;
+
+        CHECK(*cit1 == 1);
+    }
+
+    SECTION("assign-decrement by 13")
+    {
+        cit1 -= 13;
+
+        CHECK(*cit1 == 3);
+    }
+
+    SECTION("assign-decrement by -5")
+    {
+        cit1 -= -5;
+
+        CHECK(*cit1 == 3);
+    }
+
+    SECTION("add 2 to iterator")
+    {
+        auto it = cit1 + 2;
+
+        CHECK(*it == 3);
+
+        CHECK(cit1 < it);
+        CHECK((cit1 - 2) < (cit1 + 2));
+    }
+
+    SECTION("add 10 to iterator")
+    {
+        auto it = cit1 + 10;
+
+        CHECK(*it == 2);
+    }
+
+    SECTION("add -1 to iterator")
+    {
+        auto it = cit1 + -1;
+
+        CHECK(*it == 3);
+    }
+
+    SECTION("add -10 to iterator")
+    {
+        auto it = cit1 + -10;
+
+        CHECK(*it == 3);
+    }
+
+    SECTION("add iterator to 2")
+    {
+        auto it = 2 + cit1;
+
+        CHECK(*it == 3);
+    }
+
+
+    SECTION("subtract 5 from iterator")
+    {
+        auto it = cit1 - 5;
+
+        CHECK(*it == 2);
+    }
+}
