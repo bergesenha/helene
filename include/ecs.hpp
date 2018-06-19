8ab5c7861a551b6e170727cdbd2430ab95bff71c
Harald August Bergesen <bergesenha@gmail.com>
2018-06-19 12:38:03 +0200

    Add new class 'database'. 	modified:   CMakeLists.txt 	modified:   include/ecs.hpp 	modified:   tests/test_ecs.cpp

--- include/ecs.hpp
+++ include/ecs.hpp
@@ -1,6 +1,37 @@
 #pragma once
 
+#include "stable_container.hpp"
+
+#include <type_list.hpp>
+
 
 namespace helene
 {
+template <class TableTagType, TableTagType TableTag, class T>
+struct table_description
+{
+};
+
+
+namespace detail
+{
+template <class T>
+class container;
+
+
+template <class TableTagType, TableTagType TableTag, class T>
+class container<table_description<TableTagType, TableTag, T>>
+{
+protected:
+    stable_vector<T> data_;
+};
+} // namespace detail
+
+
+template <class... TableTags>
+class database : public detail::container<TableTags>...
+{
+public:
+private:
+};
 } // namespace helene
--- tests/test_ecs.cpp
+++ tests/test_ecs.cpp
@@ -3,6 +3,21 @@
 #include <ecs.hpp>
 
 
+enum class TableName
+{
+    Numbers,
+    Names
+};
+
+template <TableName Name, class T>
+using named_table = helene::table_description<TableName, Name, T>;
+
+
 TEST_CASE("", "")
 {
+    using helene::database;
+
+    database<named_table<TableName::Numbers, int>,
+             named_table<TableName::Names, std::string>>
+        db;
 }
