d3ee1ae02fa21b814a8f8f6bcc5d1a41e97062a7
Harald August Bergesen <bergesenha@gmail.com>
2018-07-13 15:09:12 +0200

    Add default constructor and destructor. 	modified:   include/small_vector.hpp 	modified:   tests/test_small_vector.cpp

--- include/small_vector.hpp
+++ include/small_vector.hpp
@@ -13,6 +13,9 @@ namespace helene
 template <class T, std::size_t StackBufferSize = sizeof(std::vector<T>*)>
 class small_vector
 {
+    static_assert(std::is_trivial<T>::value,
+                  "small_vector is only compatible with trivial types");
+
 public:
     typedef typename std::vector<T>::size_type size_type;
     typedef typename std::vector<T>::difference_type difference_type;
@@ -20,6 +23,19 @@ public:
     static constexpr size_type max_stack_size = StackBufferSize / sizeof(T);
 
 public:
+    small_vector() : size_()
+    {
+        new(&storage_) T[max_stack_size];
+    }
+
+    ~small_vector()
+    {
+        if(size_ > max_stack_size)
+        {
+            reinterpret_cast<std::vector<T>*>(&storage_)->~vector();
+        }
+    }
+
 private:
     std::aligned_storage_t<StackBufferSize> storage_;
     size_type size_;
--- tests/test_small_vector.cpp
+++ tests/test_small_vector.cpp
@@ -1,7 +1,12 @@
 #include <catch.hpp>
 #include <small_vector.hpp>
 
+struct ha
+{
+};
 
 TEST_CASE("", "")
 {
+    helene::small_vector<int> intvec;
+    helene::small_vector<ha> havec;
 }
