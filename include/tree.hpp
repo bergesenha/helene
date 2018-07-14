b29ab47d6aa5d0e702277c5425e750b45a59572c
Harald August Bergesen <bergesenha@gmail.com>
2018-07-14 15:22:57 +0200

    Add decrement operators to tree::iterator. 	modified:   include/tree.hpp

--- include/tree.hpp
+++ include/tree.hpp
@@ -66,6 +66,21 @@ public:
             return temp;
         }
 
+        iterator&
+        operator--()
+        {
+            --current_;
+            return *this;
+        }
+
+        iterator
+        operator--(int)
+        {
+            auto temp = *this;
+            --(*this);
+            return temp;
+        }
+
 
     private:
         child_tag_iterator current_;
