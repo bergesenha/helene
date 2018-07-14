362989978750b274271631503a230f145f7ac0c2
Harald August Bergesen <bergesenha@gmail.com>
2018-07-14 15:07:19 +0200

    Add unit tests. 	modified:   tests/test_tree.cpp

--- tests/test_tree.cpp
+++ tests/test_tree.cpp
@@ -26,5 +26,15 @@ TEST_CASE("", "")
 
     SECTION("insert three children of root")
     {
+        const auto b = mt.insert_node('b', root_tag);
+        const auto c = mt.insert_node('c', root_tag);
+        const auto d = mt.insert_node('d', root_tag);
+
+        auto children2 = mt.children(root_tag);
+
+        CHECK(mt[b] == 'b');
+        CHECK(mt[c] == 'c');
+        CHECK(mt[d] == 'd');
+        CHECK(std::distance(children2.first, children2.second) == 3);
     }
 }
