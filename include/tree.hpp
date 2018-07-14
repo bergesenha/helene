b261a64bc7422815a1819e264e7b36eb2ab765cc
Harald August Bergesen <bergesenha@gmail.com>
2018-07-14 14:50:03 +0200

    Add basic insertion logic. 	modified:   include/handle_map.hpp 	modified:   include/tree.hpp 	modified:   tests/test_tree.cpp

--- include/tree.hpp
+++ include/tree.hpp
@@ -12,14 +12,69 @@ template <class T>
 class tree
 {
 public:
+    typedef T value_type;
+    typedef T& reference;
+    typedef const T& const_reference;
     typedef typename handle_map<T>::handle_type node_tag_type;
 
+private:
+    static constexpr inline const node_tag_type inactive_tag =
+        std::numeric_limits<node_tag_type>::max();
+
 public:
-    tree() = default;
+    tree() : nodes_(), parents_(), children_()
+    {
+        nodes_.insert(T{});
+        parents_.push_back(inactive_tag);
+    }
+
+    tree(const T& root_node) : nodes_(), parents_(), children_()
+    {
+        nodes_.insert(root_node);
+        parents_.push_back(inactive_tag);
+    }
+
+    node_tag_type
+    insert_node(const T& value, node_tag_type parent)
+    {
+        const auto new_tag = nodes_.insert(value);
+
+        if(new_tag >= parents_.size())
+        {
+            parents_.resize(new_tag + 1, inactive_tag);
+            children_.resize(new_tag + 1);
+        }
+
+        parents_[new_tag] = parent;
+        children_[parent].push_back(new_tag);
+        return new_tag;
+    }
+
+    node_tag_type
+    parent_tag(node_tag_type node)
+    {
+        return parents_[node];
+    }
+
+    node_tag_type
+    root_tag() const
+    {
+        return node_tag_type{0};
+    }
+
+    reference operator[](node_tag_type n)
+    {
+        return nodes_[n];
+    }
+
+    const_reference operator[](node_tag_type n) const
+    {
+        return nodes_[n];
+    }
 
 private:
     handle_map<T> nodes_;
-    handle_map<node_tag_type> parents_;
-    handle_map<small_vector<node_tag_type>> children_;
+    std::vector<node_tag_type> parents_;
+    std::vector<small_vector<node_tag_type>> children_;
 };
 } // namespace helene
--- tests/test_tree.cpp
+++ tests/test_tree.cpp
@@ -4,4 +4,17 @@
 
 TEST_CASE("", "")
 {
+    helene::tree<char> mt('a');
+
+    const auto root_tag = mt.root_tag();
+
+    CHECK(mt[root_tag] == 'a');
+
+    SECTION("insert child node of root")
+    {
+        const auto b = mt.insert_node('b', root_tag);
+
+        CHECK(mt[b] == 'b');
+        CHECK(mt[root_tag] == 'a');
+    }
 }
