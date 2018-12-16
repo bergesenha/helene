5df57e90fc2b54735314158a08efaa7d791e25ca
Harald August Bergesen <bergesenha@gmail.com>
2018-12-16 22:38:09 +0100

    Add basic json classes. 	modified:   CMakeLists.txt 	modified:   include/static_json.hpp 	modified:   tests/test_static_json.cpp

--- include/static_json.hpp
+++ include/static_json.hpp
@@ -18,5 +18,28 @@ enum class value_type
     object_type
 };
 
+
+template <class NameProvider, class Type>
+struct field
+{
+};
+
+
+template <class T>
+struct holder
+{
+    T value;
+};
+
+
+template <class... Fields>
+class json;
+
+template <class... NameProviders, class... Types>
+class json<field<NameProviders, Types>...> : holder<Types>...
+{
+public:
+private:
+};
 } // namespace static_json
 } // namespace helene
--- tests/test_static_json.cpp
+++ tests/test_static_json.cpp
@@ -3,6 +3,18 @@
 #include <static_json.hpp>
 
 
+struct data_name
+{
+    static constexpr const char* value = "data";
+};
+
+
+using helene::static_json::field;
+using helene::static_json::json;
+using helene::static_json::value_type;
+
+
 TEST_CASE("", "")
 {
+    json<field<data_name, int>> js1;
 }
