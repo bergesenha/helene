6068fe109812602ac9571c559b905e351da936fd
Harald August Bergesen <bergesenha@gmail.com>
2018-12-16 22:04:59 +0100

    Add enums for value types of the json spec. 	modified:   include/static_json.hpp

--- include/static_json.hpp
+++ include/static_json.hpp
@@ -8,6 +8,15 @@ namespace helene
 namespace static_json
 {
 
+enum class value_type
+{
+    number_type,
+    bool_type,
+    string_type,
+    null_type,
+    array_type,
+    object_type
+};
 
 } // namespace static_json
 } // namespace helene
